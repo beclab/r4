b4c864cd4b2b45aa311331cf309e19421b2760ec
Ubuntu <ubuntu@ip-172-31-90-203.ec2.internal>
2024-05-16 07:48:06 +0000

    chore: mongoretore terminus mongo data for test

---
+++ train-rank/infra/rss_wise_environment/restore_mongodump_data.sh
@@ -0,0 +1,4 @@
+mongorestore  -u root -p example --host 127.0.0.1 --port 27017 --authenticationDatabase admin  /opt/knowledge/feeds.bson
+mongorestore  -u root -p example --host 127.0.0.1 --port 27017 --authenticationDatabase admin  /opt/knowledge/entries.bson
+mongorestore  -u root -p example --host 127.0.0.1 --port 27017 --authenticationDatabase admin  /opt/knowledge/algorithms.bson
+mongorestore  -u root -p example --host 127.0.0.1 --port 27017 --authenticationDatabase admin  /opt/knowledge/impressions.bson
