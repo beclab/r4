909c86749414e5860c1cbd9bb0c22a88b8f254d0
Ubuntu <ubuntu@ip-172-31-4-59.ec2.internal>
2024-12-16 09:13:56 +0000

    feat: rank: add method getImpressionForShortTermAndLongTermUserEmbeddingRank

--- train-rank/src/rssrank.h
+++ train-rank/src/rssrank.h
@@ -20,61 +20,68 @@ namespace fs = std::filesystem;
 
 DECLARE_string(recommend_source_name);
 
-namespace rssrank {
-bool trainOneBigBatch();
-bool rankLR();
-bool doRank();
-bool trainLR();
-static const float clicked_weight = 0.1;
-static const float read_finish_weight = 0.2;
-static const float read_speed_weight = 0.3;
-static const float star_weight = 0.4;
+namespace rssrank
+{
+    bool trainOneBigBatch();
+    bool rankLR();
+    bool doRank();
+    bool trainLR();
+    static const float clicked_weight = 0.1;
+    static const float read_finish_weight = 0.2;
+    static const float read_speed_weight = 0.3;
+    static const float star_weight = 0.4;
 
-enum class ModelPathType {
-  TRAINING,
-  PRODUCTION,
-};
+    enum class ModelPathType
+    {
+        TRAINING,
+        PRODUCTION,
+    };
 
-bool getCurrentBatchFromPositiveNegativeSampleWithSpeicifiedSize(
-    const std::unordered_set<std::string>& train_positive_sample,
-    const std::unordered_set<std::string>& train_negative_sample,
-    std::vector<std::string>* current_batch, int positive_batch_size,
-    int negative_batch_size);
+    bool getCurrentBatchFromPositiveNegativeSampleWithSpeicifiedSize(
+        const std::unordered_set<std::string> &train_positive_sample,
+        const std::unordered_set<std::string> &train_negative_sample,
+        std::vector<std::string> *current_batch, int positive_batch_size,
+        int negative_batch_size);
 
-const int data_iter_batch_size = 128;
-const int data_iter_positive_batch_size = 32;
-const int data_iter_negative_batch_size = 96;
+    const int data_iter_batch_size = 128;
+    const int data_iter_positive_batch_size = 32;
+    const int data_iter_negative_batch_size = 96;
 
-std::string getRankModelPath(ModelPathType model_path_type);
-int getCurrentEmbeddingDimension();
+    std::string getRankModelPath(ModelPathType model_path_type);
+    int getCurrentEmbeddingDimension();
 
-std::unordered_map<std::string, float> inferPredictVector(
-    const std::vector<float>& test_features, int test_rows,
-    BoosterHandle h_booster, const std::vector<std::string>& id_list);
-void rankPredict();
+    std::unordered_map<std::string, float> inferPredictVector(
+        const std::vector<float> &test_features, int test_rows,
+        BoosterHandle h_booster, const std::vector<std::string> &id_list);
+    void rankPredict();
 
-void getEachImpressionScoreKnowledge(
-    std::unordered_map<std::string, float>* positive,
-    std::unordered_map<std::string, float>* negative);
-float getSpecificImpressionScore(const Impression& current_impression);
-enum FillFeatureSource { ALGORITHM, IMPRESSION };
+    void getEachImpressionScoreKnowledge(
+        std::unordered_map<std::string, float> *positive,
+        std::unordered_map<std::string, float> *negative);
+    float getSpecificImpressionScore(const Impression &current_impression);
+    enum FillFeatureSource
+    {
+        ALGORITHM,
+        IMPRESSION
+    };
 
-bool fillFeaturesKnowledgeVector(std::vector<std::vector<float>>& features,
-                                 const std::vector<std::string>& ids,
-                                 enum FillFeatureSource embedding_source);
+    bool fillFeaturesKnowledgeVector(std::vector<std::vector<float>> &features,
+                                     const std::vector<std::string> &ids,
+                                     enum FillFeatureSource embedding_source);
 
-std::unordered_map<std::string, std::string> getNotRankedAlgorithmToEntry();
-std::unordered_map<std::string, ScoreWithMetadata>
-getAllEntryToPrerankSourceForCurrentSourceKnowledge();
+    std::unordered_map<std::string, std::string> getNotRankedAlgorithmToEntry();
+    std::unordered_map<std::string, ScoreWithMetadata>
+    getAllEntryToPrerankSourceForCurrentSourceKnowledge();
 
-bool trainOneBigBatchWithPreparedDataVector(
-    const std::vector<float>& features, float* train_labels, const int rows,
-    const std::unordered_map<std::string, std::string>& parameters,
-    const std::vector<float>& test_features, int test_rows, float* test_labels,
-    float* biggest_auc);
+    bool trainOneBigBatchWithPreparedDataVector(
+        const std::vector<float> &features, float *train_labels, const int rows,
+        const std::unordered_map<std::string, std::string> &parameters,
+        const std::vector<float> &test_features, int test_rows, float *test_labels,
+        float *biggest_auc);
 
-bool trainOneBigBatchPredictVector(const std::vector<float>& test_features,
-                                   int test_rows, float* test_labels,
-                                   BoosterHandle h_booster, float* biggest_auc);
+    bool trainOneBigBatchPredictVector(const std::vector<float> &test_features,
+                                       int test_rows, float *test_labels,
+                                       BoosterHandle h_booster, float *biggest_auc);
+    std::vector<Impression> getImpressionForShortTermAndLongTermUserEmbeddingRank();
 
-}  // namespace rssrank
+} // namespace rssrank
