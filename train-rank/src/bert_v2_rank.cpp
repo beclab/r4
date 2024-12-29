5c4585c31a41104f932483eaa32ea50d163cb9f0
Ubuntu <ubuntu@ip-172-31-4-59.ec2.internal>
2024-12-29 20:17:08 +0000

    feat:1. for train do nothing 2. for rank use rankShortTermAndLongTermUserEmbedding

--- train-rank/src/bert_v2_rank.cpp
+++ train-rank/src/bert_v2_rank.cpp
@@ -8,7 +8,8 @@
 #include "rssrank.h"
 #include "common_tool.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   init_log();
   LOG(INFO) << "Git rev: " << GIT_REV;
@@ -16,14 +17,16 @@ int main(int argc, char** argv) {
   LOG(INFO) << "Git branch: " << GIT_BRANCH;
 
   const char *source_name = std::getenv(TERMINUS_RECOMMEND_SOURCE_NAME);
-  if (source_name == nullptr) {
+  if (source_name == nullptr)
+  {
     LOG(ERROR) << TERMINUS_RECOMMEND_SOURCE_NAME << " NOT EXIST" << std::endl;
     exit(-1);
   }
   LOG(DEBUG) << TERMINUS_RECOMMEND_SOURCE_NAME << source_name << std::endl;
 
   const char *knowledge_base_api_uri = std::getenv(KNOWLEDGE_BASE_API_URL);
-  if (knowledge_base_api_uri == nullptr) {
+  if (knowledge_base_api_uri == nullptr)
+  {
     LOG(ERROR) << KNOWLEDGE_BASE_API_URL << " NOT EXIST" << std::endl;
     exit(-1);
   }
@@ -31,7 +34,8 @@ int main(int argc, char** argv) {
              << std::endl;
   setenv(TERMINUS_RECOMMEND_EMBEDDING_NAME, "bert_v2", 0);
   setenv(TERMINUS_RECOMMEND_EMBEDDING_DIMENSION, "384", 0);
-  //rssrank::rankPredict();
-  rssrank::rankLR();
+  // rssrank::rankPredict();
+  //  rssrank::rankLR();
+  rssrank::rankShortTermAndLongTermUserEmbedding();
   LOG(DEBUG) << "compelete rank" << std::endl;
 }
